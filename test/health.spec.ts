573420580ecfc0b7c4fab2c1360c1557363d2b97
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-29 11:42:43 +0100

    init lib

---
+++ test/health.spec.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021 Aleix Morgadas <aleix@symboldapp.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+describe('Health Test', () => {
+    test('it is always true', () => {
+        expect(true).toBeTruthy();
+    })
+})
